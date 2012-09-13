e22150cd4c91acd15b1503891cf77b0f3e49b634
Xanagit <xanagit@gmail.com>
2012-09-13 18:48:15 +0200

    grobid
    Former-commit-id: 9b3f7a73af8968ceabe0e01a3aa899b549e3f988

---
+++ grobid-service/src/main/java/org/grobid/service/GrobidPathes.java
@@ -0,0 +1,64 @@
+/**
+ * Copyright 2010 INRIA.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *
+ */
+package org.grobid.service;
+
+/**
+ * This interface only contains the path extensions for accessing the grobid service.
+ * @author Florian Zipser
+ *
+ */
+public interface GrobidPathes {
+	/**
+	 * path extension for grobid service.
+	 */
+	public static final String PATH_GROBID= "grobid";
+	
+	/**
+	 * path extension for is alive request.
+	 */
+	public static final String PATH_IS_ALIVE= "isalive";
+	/**
+	 * path extension for grobid admin pages.
+	 */
+	public static final String PATH_ADMIN= "admin/{pw}";
+	
+	/**
+	 * path extension for processing document headers.
+	 */
+	public static final String PATH_HEADER= "processHeaderDocument";
+	/**
+	 * path extension for processing full text of documents.
+	 */
+	public static final String PATH_FULL_TEXT= "processFulltextDocument";
+	/**
+	 * path extension for processing dates.
+	 */
+	public static final String PATH_DATE= "processDate";
+	/**
+	 * path extension for processing names in header parts of documents headers.
+	 */
+	public static final String PATH_HEADER_NAMES= "processHeaderNames";
+	/**
+	 * path extension for processing names as citations in full text part of documents.
+	 */
+	public static final String PATH_CITE_NAMES= "processCitationNames";
+	/**
+	 * path extension for processing affiliation in document headers.
+	 */
+	public static final String PATH_AFFILIATION= "processAffiliations";
+}
