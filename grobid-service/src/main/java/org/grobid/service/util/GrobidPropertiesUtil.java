08db70798544ec009428c52735b61bed57c38ae2
Xanagit <xanagit@gmail.com>
2012-11-04 18:37:40 +0100

    Adding some methods to deal with GrobidProperty.
    Former-commit-id: e9ad8d710774885f07f813fa4e020b75121588df

--- grobid-service/src/main/java/org/grobid/service/util/GrobidPropertiesUtil.java
+++ grobid-service/src/main/java/org/grobid/service/util/GrobidPropertiesUtil.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Properties;
 
 import org.grobid.core.utilities.GrobidProperties;
+import org.grobid.core.utilities.XmlUtils;
 
 /**
  * Utility methods for managing Grobid properties.
@@ -14,6 +15,12 @@ import org.grobid.core.utilities.GrobidProperties;
  */
 public class GrobidPropertiesUtil {
 
+	/**
+	 * Return a list of {@link GrobidProperty} containing all the properties of
+	 * Grobid.
+	 * 
+	 * @return List<GrobidProperty>
+	 */
 	public static List<GrobidProperty> getAllPropertiesList() {
 		List<GrobidProperty> gbdProperties = new ArrayList<GrobidProperty>();
 		Properties props = GrobidServiceProperties.getProps();
@@ -31,6 +38,19 @@ public class GrobidPropertiesUtil {
 		return gbdProperties;
 	}
 
-	
+	/**
+	 * Build an xml representation of all grobid properties.
+	 * 
+	 * @return String
+	 */
+	public static String getAllPropertiesListXml() {
+		StringBuilder gbdProperties = new StringBuilder();
+		gbdProperties.append(XmlUtils.startTag("properties"));
+		for (GrobidProperty currProp : getAllPropertiesList()) {
+			gbdProperties.append(currProp.toString());
+		}
+		gbdProperties.append(XmlUtils.endTag("properties"));
+		return gbdProperties.toString();
+	}
 
 }
