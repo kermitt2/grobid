910f61eba3e98b9c56c7c2afe6652201c9d0f37f
Xanagit <xanagit@gmail.com>
2012-11-16 11:24:51 +0100

    Code formatting.

--- grobid-core/src/test/java/org/grobid/core/utilities/UtilitiesTest.java
+++ grobid-core/src/test/java/org/grobid/core/utilities/UtilitiesTest.java
@@ -1,42 +1,70 @@
 package org.grobid.core.utilities;
 
+import java.io.File;
+import java.io.IOException;
+
 import org.junit.Test;
-import org.junit.Assert;
+import static org.junit.Assert.*;
 
 public class UtilitiesTest {
-	
+
 	@Test
-	public void testStringToBooleanTrue(){
-		Assert.assertEquals("stringToBoolean value does not match expected result", true,Utilities.stringToBoolean("true"));
-	}
-	
-	@Test
-	public void testStringToBooleanTrue2(){
-		Assert.assertEquals("stringToBoolean value does not match expected result", true,Utilities.stringToBoolean(" TruE "));
+	public void testStringToBooleanTrue() {
+		assertEquals(
+				"stringToBoolean value does not match expected result", true,
+				Utilities.stringToBoolean("true"));
 	}
 
 	@Test
-	public void testStringToBooleanFalse(){
-		Assert.assertEquals("stringToBoolean value does not match expected result", false,Utilities.stringToBoolean("false"));
+	public void testStringToBooleanTrue2() {
+		assertEquals(
+				"stringToBoolean value does not match expected result", true,
+				Utilities.stringToBoolean(" TruE "));
 	}
-	
+
 	@Test
-	public void testStringToBooleanFalse2(){
-		Assert.assertEquals("stringToBoolean value does not match expected result", false,Utilities.stringToBoolean(" fAlSe "));
+	public void testStringToBooleanFalse() {
+		assertEquals(
+				"stringToBoolean value does not match expected result", false,
+				Utilities.stringToBoolean("false"));
 	}
-	
+
 	@Test
-	public void testStringToBooleanFalse3(){
-		Assert.assertEquals("stringToBoolean value does not match expected result", false,Utilities.stringToBoolean(" non boolean value"));
+	public void testStringToBooleanFalse2() {
+		assertEquals(
+				"stringToBoolean value does not match expected result", false,
+				Utilities.stringToBoolean(" fAlSe "));
 	}
-	
+
 	@Test
-	public void testStringToBooleanBlank(){
-		Assert.assertEquals("stringToBoolean value does not match expected result", false,Utilities.stringToBoolean(""));
+	public void testStringToBooleanFalse3() {
+		assertEquals(
+				"stringToBoolean value does not match expected result", false,
+				Utilities.stringToBoolean(" non boolean value"));
 	}
-	
+
 	@Test
-	public void testStringToBooleanBlank2(){
-		Assert.assertEquals("stringToBoolean value does not match expected result", false,Utilities.stringToBoolean(null));
+	public void testStringToBooleanBlank() {
+		assertEquals(
+				"stringToBoolean value does not match expected result", false,
+				Utilities.stringToBoolean(""));
+	}
+
+	@Test
+	public void testStringToBooleanBlank2() {
+		assertEquals(
+				"stringToBoolean value does not match expected result", false,
+				Utilities.stringToBoolean(null));
+	}
+
+	@Test
+	public void testwriteInFileANDreadFile() throws IOException {
+		File file = File.createTempFile("temp", "test");
+		Utilities.writeInFile(file.getAbsolutePath(), getString());
+		assertEquals("Not expected value", getString(), Utilities.readFile(file.getAbsolutePath()));
+	}
+
+	private static String getString() {
+		return "1 \" ' A \n \t \r test\n\\n \n M";
 	}
 }
