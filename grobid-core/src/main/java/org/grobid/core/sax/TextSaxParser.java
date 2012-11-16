1ecc10f7b71e17d844e65a4b46ad193091315e59
Xanagit <xanagit@gmail.com>
2012-11-16 11:21:37 +0100

    Code formatting.

--- grobid-core/src/main/java/org/grobid/core/sax/TextSaxParser.java
+++ grobid-core/src/main/java/org/grobid/core/sax/TextSaxParser.java
@@ -6,84 +6,83 @@ import org.xml.sax.helpers.*;
 /**
  * Stupid SAX parser which accumulate the textual content.
  * <p/>
- * As an option, it is possible to accumulate only the content under a given element name, for instance
- * "description" for getting the description of a patent XML document.
- *
+ * As an option, it is possible to accumulate only the content under a given
+ * element name, for instance "description" for getting the description of a
+ * patent XML document.
+ * 
  * @author Patrice Lopez
  */
 public class TextSaxParser extends DefaultHandler {
 
-    StringBuffer accumulator = new StringBuffer(); // Accumulate parsed text
+	StringBuffer accumulator = new StringBuffer(); // Accumulate parsed text
 
-    private String filter = null; // the name of an element for getting only the corresponding text
+	private String filter = null; // the name of an element for getting only the
+									// corresponding text
 
-    private boolean accumule = true;
+	private boolean accumule = true;
 
-    public String currentPatentNumber = null;
-    public String country = null;
+	public String currentPatentNumber = null;
+	public String country = null;
 
-    public TextSaxParser() {
-    }
+	public TextSaxParser() {
+	}
 
-    public void characters(char[] buffer, int start, int length) {
-        if (accumule) {
-            accumulator.append(buffer, start, length);
-        }
-    }
+	public void characters(char[] buffer, int start, int length) {
+		if (accumule) {
+			accumulator.append(buffer, start, length);
+		}
+	}
 
-    public void setFilter(String filt) {
-        filter = filt;
-        accumule = false;
-    }
+	public void setFilter(String filt) {
+		filter = filt;
+		accumule = false;
+	}
 
-    public String getText() {
-        String text = accumulator.toString().trim();
-        text = text.replace("\n", " ");
-        text = text.replace("\t", " ");
-        text = text.replace("  ", " ");
-        return text;
-    }
+	public String getText() {
+		String text = accumulator.toString().trim();
+		text = text.replace("\n", " ");
+		text = text.replace("\t", " ");
+		text = text.replaceAll("\\p{Space}+", " ");
+		return text;
+	}
 
-    public void endElement(java.lang.String uri,
-                           java.lang.String localName,
-                           java.lang.String qName) throws SAXException {
-        if (qName.equals(filter)) {
-            accumule = false;
-        }
-        if (accumule) {
-            if (qName.equals("row") || qName.equals("p")) {
-                accumulator.append(" ");
-            }
-        }
-    }
+	public void endElement(String uri, String localName, String qName)
+			throws SAXException {
+		if (qName.equals(filter)) {
+			accumule = false;
+		}
+		if (accumule) {
+			if (qName.equals("row") || qName.equals("p")) {
+				accumulator.append(" ");
+			}
+		}
+	}
 
-    public void startElement(String namespaceURI,
-                             String localName,
-                             String qName,
-                             Attributes atts) throws SAXException {
-        if (qName.equals("patent-document")) {
-            int length = atts.getLength();
+	public void startElement(String namespaceURI, String localName,
+			String qName, Attributes atts) throws SAXException {
+		if (qName.equals("patent-document")) {
+			int length = atts.getLength();
 
-            // Process each attribute
-            for (int i = 0; i < length; i++) {
-                // Get names and values for each attribute
-                String name = atts.getQName(i);
-                String value = atts.getValue(i);
+			// Process each attribute
+			for (int i = 0; i < length; i++) {
+				// Get names and values for each attribute
+				String name = atts.getQName(i);
+				String value = atts.getValue(i);
 
-                if (name != null) {
-                    if (name.equals("country")) {
-                        country = value;
-                    }
-                    if (name.equals("doc-number")) {
-                        currentPatentNumber = country + value;
-                    }
-                }
-            }
-        }
+				if (name != null) {
+					if (name.equals("country")) {
+						country = value;
+					}
+					if (name.equals("doc-number")) {
+						currentPatentNumber = country + value;
+					}
+				}
+			}
+		}
 
-        if (qName.equals(filter)) {
-            accumule = true;
-        }
-    }
+		if (qName.equals(filter)) {
+			accumule = true;
+		}
+	}
 
 }
