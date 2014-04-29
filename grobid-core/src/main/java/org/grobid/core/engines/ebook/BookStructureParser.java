4dccfdfb3e46c979b94db2434dc7eff04f33b6b1
Vyacheslav Zholudev <vyacheslav.zholudev@gmail.com>
2014-04-29 15:41:33 +0200

    updating grobid-core version
    Former-commit-id: 4f718a6c395dda3c8b650c50918c94839be18b92

--- grobid-core/src/main/java/org/grobid/core/engines/ebook/BookStructureParser.java
+++ grobid-core/src/main/java/org/grobid/core/engines/ebook/BookStructureParser.java
@@ -19,7 +19,6 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
 import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /**
  * Structure extraction of an ebook.
