95f1690c35fa1daf19fcac5453a4e4c9491974b1
kermitt2 <patrice.lopez@inria.fr>
2015-04-19 07:16:27 +0200

    Update reference-segmenter resources
    Former-commit-id: 5d9d2c89434731fe2a3280c34c067668a158a1e2

--- grobid-trainer/src/main/java/org/grobid/trainer/WapitiTrainer.java
+++ grobid-trainer/src/main/java/org/grobid/trainer/WapitiTrainer.java
@@ -17,16 +17,16 @@ public class WapitiTrainer implements GenericTrainer {
     @Override
     public void train(File template, File trainingData, File outputModel, int numThreads, GrobidModels model) {
 		// parameters
-		double eta = 0.00001; // default similar to CRF++
+		double epsilon = 0.00001; // default size of the interval for stopping criterion
 		int window = 20; // default similar to CRF++
 		// adjusting depending on the model to be trained
 		if (model.getModelName().equals("segmentation")) {
-			eta = SegmentationTrainer.eta;
+			epsilon = SegmentationTrainer.epsilon;
 			window = SegmentationTrainer.window;
 		}
         WapitiModel.train(template, trainingData, outputModel, "--nthread " + numThreads +
 //       		" --algo sgd-l1"
-			" -e " + eta +
+			" -e " + epsilon +
 			" -w " + window +
 			""
         );
