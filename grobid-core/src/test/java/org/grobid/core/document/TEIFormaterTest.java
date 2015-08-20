4582d9c5a02705e239cbeeed7d201e0fa22634bc
kermitt2 <patrice.lopez@inria.fr>
2015-08-20 12:57:56 +0200

    Fix for issue #65

--- grobid-core/src/test/java/org/grobid/core/document/TEIFormaterTest.java
+++ grobid-core/src/test/java/org/grobid/core/document/TEIFormaterTest.java
@@ -18,14 +18,18 @@ import org.junit.Test;
  */
 public class TEIFormaterTest {
 
-    //@Test
+    @Test
     public void testRegex() {
 		Pattern localNumberRefCompact =
-		            Pattern.compile("(\\[|\\()(\\d)+(\\w)?(\\-\\d+\\w?)?((,\\s?)(\\d)+(\\w)?(\\-\\d+\\w?)?)s*(\\)|\\])");
+		    		//Pattern.compile("(\\[|\\()((\\d+\\w?)(\\-(\\d+\\w?))?(,|;)?\\s?)+(\\)|\\])?");
+			Pattern.compile("(\\[|\\()((\\d)+(\\w)?(\\-\\d+\\w?)?,\\s?)+(\\d+\\w?)(\\-\\d+\\w?)?(\\)|\\])");
 		System.out.println(localNumberRefCompact.pattern());
 		String text = "All selected (2000, 2100, 2900, 2902, 2908, 2909, 4300, 8200, 8900, 8902, 9720, 9730, 9780, 9790, 9900,17100,17202,17230,17231,17232, 17239,17290,17292,17293,17320,17330, 17360,17390,17901, 80000";
-		Matcher m = localNumberRefCompact.matcher(text);
-		m.find();
+		text = TEIFormater.bracketReferenceSegment(text);
+		if (text != null) {
+			Matcher m = localNumberRefCompact.matcher(text);
+			m.find();
+		}
 		System.out.println("Done");
     }
 
