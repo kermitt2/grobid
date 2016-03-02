8b70acc4dc4658eb59682c00a7db5c5b22b4f9b8
Luca Foppiano <luca@foppiano.org>
2016-03-03 00:34:46 +0100

    Added constructor to simplify the cration of an OffestPosition
    Former-commit-id: fe64fc28d0e4c23ac532b428da2c5f76ba722e6b

--- grobid-core/src/main/java/org/grobid/core/utilities/OffsetPosition.java
+++ grobid-core/src/main/java/org/grobid/core/utilities/OffsetPosition.java
@@ -4,6 +4,14 @@ public class OffsetPosition {
     public int start = -1;
     public int end = -1;
 
+    public OffsetPosition() {
+    }
+
+    public OffsetPosition(int start, int end) {
+        this.start = start;
+        this.end = end;
+    }
+
     public String toString() {
         return "" + start + "\t" + end;
     }
