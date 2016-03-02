549d4bf8148494d28e359555b9243d6339ea6f01
Luca Foppiano <luca@foppiano.org>
2016-03-03 00:33:58 +0100

    Make the training parameter being used by the superclass when running the train
    Former-commit-id: 947ea0ad6a44aac65a994ea5c72ce7c9c14ed7c4

--- grobid-trainer/src/main/java/org/grobid/trainer/FigureTrainer.java
+++ grobid-trainer/src/main/java/org/grobid/trainer/FigureTrainer.java
@@ -16,72 +16,65 @@ import java.util.StringTokenizer;
 /**
  * @author Patrice Lopez
  */
-public class FigureTrainer extends AbstractTrainer{
+public class FigureTrainer extends AbstractTrainer {
 
     public FigureTrainer() {
         super(GrobidModels.FIGURE);
-    }
 
-	// adjusting CRF training parameters for this model (only with Wapiti)
-	private double epsilon = 0.00001;
-	private int window = 20;
+        // adjusting CRF training parameters for this model (only with Wapiti)
+        epsilon = 0.00001;
+        window = 20;
+    }
 
     @Override
     public int createCRFPPData(File corpusPath, File outputFile) {
-        return addFeaturesFigure(corpusPath.getAbsolutePath() + "/tei", 
-								 corpusPath.getAbsolutePath() + "/raw", 
-								 outputFile, null, 1.0);
+        return addFeaturesFigure(corpusPath.getAbsolutePath() + "/tei",
+                corpusPath.getAbsolutePath() + "/raw",
+                outputFile, null, 1.0);
     }
-
-	/**
-	 * Add the selected features for the figure model 
-	 * 
-	 * @param corpusDir
-	 *            path where corpus files are located
-	 * @param trainingOutputPath
-	 *            path where to store the temporary training data
-	 * @param evalOutputPath
-	 *            path where to store the temporary evaluation data
-	 * @param splitRatio
-	 *            ratio to consider for separating training and evaluation data, e.g. 0.8 for 80% 
-	 * @return the total number of used corpus items 
-	 */
-	@Override
-	public int createCRFPPData(final File corpusDir, 
-							final File trainingOutputPath, 
-							final File evalOutputPath, 
-							double splitRatio) {
-		return addFeaturesFigure(corpusDir.getAbsolutePath() + "/tei", 
-								corpusDir.getAbsolutePath() + "/raw", 
-								trainingOutputPath, 
-								evalOutputPath, 
-								splitRatio);				
-	}
 
     /**
      * Add the selected features for the figure model
-	 *
+     *
+     * @param corpusDir          path where corpus files are located
+     * @param trainingOutputPath path where to store the temporary training data
+     * @param evalOutputPath     path where to store the temporary evaluation data
+     * @param splitRatio         ratio to consider for separating training and evaluation data, e.g. 0.8 for 80%
+     * @return the total number of used corpus items
+     */
+    @Override
+    public int createCRFPPData(final File corpusDir,
+                               final File trainingOutputPath,
+                               final File evalOutputPath,
+                               double splitRatio) {
+        return addFeaturesFigure(corpusDir.getAbsolutePath() + "/tei",
+                corpusDir.getAbsolutePath() + "/raw",
+                trainingOutputPath,
+                evalOutputPath,
+                splitRatio);
+    }
+
+    /**
+     * Add the selected features for the figure model
+     *
      * @param sourceTEIPathLabel path to corpus TEI files
-     * @param sourceRawPathLabel path to corpus raw files	
-     * @param trainingOutputPath
-	 *            path where to store the temporary training data
-	 * @param evalOutputPath
-	 *            path where to store the temporary evaluation data
-	 * @param splitRatio
-	 *            ratio to consider for separating training and evaluation data, e.g. 0.8 for 80%
+     * @param sourceRawPathLabel path to corpus raw files
+     * @param trainingOutputPath path where to store the temporary training data
+     * @param evalOutputPath     path where to store the temporary evaluation data
+     * @param splitRatio         ratio to consider for separating training and evaluation data, e.g. 0.8 for 80%
      * @return number of examples
      */
     public int addFeaturesFigure(String sourceTEIPathLabel,
-                                   String sourceRawPathLabel,
-		   						   final File trainingOutputPath, 
-		   						   final File evalOutputPath, 
-								   double splitRatio) {
+                                 String sourceRawPathLabel,
+                                 final File trainingOutputPath,
+                                 final File evalOutputPath,
+                                 double splitRatio) {
         int totalExamples = 0;
         try {
             System.out.println("sourceTEIPathLabel: " + sourceTEIPathLabel);
             System.out.println("sourceRawPathLabel: " + sourceRawPathLabel);
             System.out.println("trainingOutputPath: " + trainingOutputPath);
-			System.out.println("evalOutputPath: " + evalOutputPath);
+            System.out.println("evalOutputPath: " + evalOutputPath);
 
             // we need first to generate the labeled files from the TEI annotated files
             File input = new File(sourceTEIPathLabel);
@@ -98,32 +91,32 @@ public class FigureTrainer extends AbstractTrainer{
 
             System.out.println(refFiles.length + " tei files");
 
-			// the file for writing the training data
-			OutputStream os2 = null;
-			Writer writer2 = null;
-			if (trainingOutputPath != null) {
-				os2 = new FileOutputStream(trainingOutputPath);
-				writer2 = new OutputStreamWriter(os2, "UTF8");
-			}
-		
-			// the file for writing the evaluation data
-			OutputStream os3 = null;
-			Writer writer3 = null;
-			if (evalOutputPath != null) {
-				os3 = new FileOutputStream(evalOutputPath);
-				writer3 = new OutputStreamWriter(os3, "UTF8");
-			}
+            // the file for writing the training data
+            OutputStream os2 = null;
+            Writer writer2 = null;
+            if (trainingOutputPath != null) {
+                os2 = new FileOutputStream(trainingOutputPath);
+                writer2 = new OutputStreamWriter(os2, "UTF8");
+            }
+
+            // the file for writing the evaluation data
+            OutputStream os3 = null;
+            Writer writer3 = null;
+            if (evalOutputPath != null) {
+                os3 = new FileOutputStream(evalOutputPath);
+                writer3 = new OutputStreamWriter(os3, "UTF8");
+            }
 
             // get a factory for SAX parser
             SAXParserFactory spf = SAXParserFactory.newInstance();
-			
+
             for (File tf : refFiles) {
                 String name = tf.getName();
                 System.out.println(name);
 
-				// the full text SAX parser can be reused for the figures
+                // the full text SAX parser can be reused for the figures
                 TEIFigureSaxParser parser2 = new TEIFigureSaxParser();
-				//parser2.setMode(TEIFulltextSaxParser.FIGURE);
+                //parser2.setMode(TEIFulltextSaxParser.FIGURE);
 
                 //get a new instance of parser
                 SAXParser p = spf.newSAXParser();
@@ -131,22 +124,22 @@ public class FigureTrainer extends AbstractTrainer{
 
                 List<String> labeled = parser2.getLabeledResult();
                 //totalExamples += parser2.n;
-                
-				// we can now add the features
+
+                // we can now add the features
                 // we open the featured file
-				File theRawFile = new File(sourceRawPathLabel + File.separator + name.replace(".tei.xml", ""));
-				if (!theRawFile.exists()) {
-	                System.out.println("Raw file " + theRawFile +
-	                        " does not exist. Please have a look!");
-					continue;
-				}			
+                File theRawFile = new File(sourceRawPathLabel + File.separator + name.replace(".tei.xml", ""));
+                if (!theRawFile.exists()) {
+                    System.out.println("Raw file " + theRawFile +
+                            " does not exist. Please have a look!");
+                    continue;
+                }
                 int q = 0;
                 BufferedReader bis = new BufferedReader(
                         new InputStreamReader(new FileInputStream(theRawFile), "UTF8"));
 
                 StringBuilder figure = new StringBuilder();
                 String line;
-                while ((line = bis.readLine()) != null) {                 
+                while ((line = bis.readLine()) != null) {
                     int ii = line.indexOf('\t');
                     if (ii == -1) {
                         ii = line.indexOf(' ');
@@ -157,10 +150,10 @@ public class FigureTrainer extends AbstractTrainer{
                     // we get the label in the labelled data file for the same token
                     for (int pp = q; pp < labeled.size(); pp++) {
                         String localLine = labeled.get(pp);
-						if (localLine.trim().length() == 0) {
-							figure.append("\n");
-							continue;
-						}
+                        if (localLine.trim().length() == 0) {
+                            figure.append("\n");
+                            continue;
+                        }
                         StringTokenizer st = new StringTokenizer(localLine, " \t");
                         if (st.hasMoreTokens()) {
                             String localToken = st.nextToken();
@@ -178,28 +171,28 @@ public class FigureTrainer extends AbstractTrainer{
                     }
                 }
                 bis.close();
-	
-				if ( (writer2 == null) && (writer3 != null) )
-					writer3.write(figure.toString() + "\n");
-				if ( (writer2 != null) && (writer3 == null) )
-					writer2.write(figure.toString() + "\n");
-				else {		
-					if (Math.random() <= splitRatio)
-						writer2.write(figure.toString() + "\n");
-					else 
-						writer3.write(figure.toString() + "\n");
-				}
+
+                if ((writer2 == null) && (writer3 != null))
+                    writer3.write(figure.toString() + "\n");
+                if ((writer2 != null) && (writer3 == null))
+                    writer2.write(figure.toString() + "\n");
+                else {
+                    if (Math.random() <= splitRatio)
+                        writer2.write(figure.toString() + "\n");
+                    else
+                        writer3.write(figure.toString() + "\n");
+                }
             }
 
             if (writer2 != null) {
-				writer2.close();
-				os2.close();
-			}
-			
-			if (writer3 != null) {
-				writer3.close();
-				os3.close();
-			}
+                writer2.close();
+                os2.close();
+            }
+
+            if (writer3 != null) {
+                writer3.close();
+                os3.close();
+            }
         } catch (Exception e) {
             throw new GrobidException("An exception occured while running training for the figure model.", e);
         }
@@ -210,11 +203,11 @@ public class FigureTrainer extends AbstractTrainer{
      * Command line execution.
      *
      * @param args Command line arguments.
-     * @throws Exception 
+     * @throws Exception
      */
     public static void main(String[] args) throws Exception {
-    	MockContext.setInitialContext();
-    	GrobidProperties.getInstance();
+        MockContext.setInitialContext();
+        GrobidProperties.getInstance();
         AbstractTrainer.runTraining(new FigureTrainer());
         AbstractTrainer.runEvaluation(new FigureTrainer());
         MockContext.destroyInitialContext();
--- grobid-trainer/src/main/java/org/grobid/trainer/SegmentationTrainer.java
+++ grobid-trainer/src/main/java/org/grobid/trainer/SegmentationTrainer.java
@@ -16,72 +16,65 @@ import java.util.StringTokenizer;
 /**
  * @author Patrice Lopez
  */
-public class SegmentationTrainer extends AbstractTrainer{
+public class SegmentationTrainer extends AbstractTrainer {
 
     public SegmentationTrainer() {
         super(GrobidModels.SEGMENTATION);
-    }
 
-	// adjusting CRF training parameters for this model (only with Wapiti)
-	private double epsilon = 0.00001;
-	private int window = 20;
+        // adjusting CRF training parameters for this model (only with Wapiti)
+        epsilon = 0.00001;
+        window = 20;
+    }
 
     @Override
     public int createCRFPPData(File corpusPath, File outputFile) {
-        return addFeaturesSegmentation(corpusPath.getAbsolutePath() + "/tei", 
-									corpusPath.getAbsolutePath() + "/raw", 
-									outputFile, null, 1.0);
+        return addFeaturesSegmentation(corpusPath.getAbsolutePath() + "/tei",
+                corpusPath.getAbsolutePath() + "/raw",
+                outputFile, null, 1.0);
     }
-
-	/**
-	 * Add the selected features for the segmentation model 
-	 * 
-	 * @param corpusDir
-	 *            path where corpus files are located
-	 * @param trainingOutputPath
-	 *            path where to store the temporary training data
-	 * @param evalOutputPath
-	 *            path where to store the temporary evaluation data
-	 * @param splitRatio
-	 *            ratio to consider for separating training and evaluation data, e.g. 0.8 for 80% 
-	 * @return the total number of used corpus items 
-	 */
-	@Override
-	public int createCRFPPData(final File corpusDir, 
-							final File trainingOutputPath, 
-							final File evalOutputPath, 
-							double splitRatio) {
-		return addFeaturesSegmentation(corpusDir.getAbsolutePath() + "/tei", 
-								corpusDir.getAbsolutePath() + "/raw", 
-								trainingOutputPath, 
-								evalOutputPath, 
-								splitRatio);				
-	}
 
     /**
      * Add the selected features for the segmentation model
-	 *
+     *
+     * @param corpusDir          path where corpus files are located
+     * @param trainingOutputPath path where to store the temporary training data
+     * @param evalOutputPath     path where to store the temporary evaluation data
+     * @param splitRatio         ratio to consider for separating training and evaluation data, e.g. 0.8 for 80%
+     * @return the total number of used corpus items
+     */
+    @Override
+    public int createCRFPPData(final File corpusDir,
+                               final File trainingOutputPath,
+                               final File evalOutputPath,
+                               double splitRatio) {
+        return addFeaturesSegmentation(corpusDir.getAbsolutePath() + "/tei",
+                corpusDir.getAbsolutePath() + "/raw",
+                trainingOutputPath,
+                evalOutputPath,
+                splitRatio);
+    }
+
+    /**
+     * Add the selected features for the segmentation model
+     *
      * @param sourceTEIPathLabel path to corpus TEI files
-     * @param sourceRawPathLabel path to corpus raw files	
-     * @param trainingOutputPath
-	 *            path where to store the temporary training data
-	 * @param evalOutputPath
-	 *            path where to store the temporary evaluation data
-	 * @param splitRatio
-	 *            ratio to consider for separating training and evaluation data, e.g. 0.8 for 80%
+     * @param sourceRawPathLabel path to corpus raw files
+     * @param trainingOutputPath path where to store the temporary training data
+     * @param evalOutputPath     path where to store the temporary evaluation data
+     * @param splitRatio         ratio to consider for separating training and evaluation data, e.g. 0.8 for 80%
      * @return number of examples
      */
     public int addFeaturesSegmentation(String sourceTEIPathLabel,
-                                   String sourceRawPathLabel,
-		   						   final File trainingOutputPath, 
-		   						   final File evalOutputPath, 
-								   double splitRatio) {
+                                       String sourceRawPathLabel,
+                                       final File trainingOutputPath,
+                                       final File evalOutputPath,
+                                       double splitRatio) {
         int totalExamples = 0;
         try {
             System.out.println("sourceTEIPathLabel: " + sourceTEIPathLabel);
             System.out.println("sourceRawPathLabel: " + sourceRawPathLabel);
             System.out.println("trainingOutputPath: " + trainingOutputPath);
-			System.out.println("evalOutputPath: " + evalOutputPath);
+            System.out.println("evalOutputPath: " + evalOutputPath);
 
             // we need first to generate the labeled files from the TEI annotated files
             File input = new File(sourceTEIPathLabel);
@@ -98,21 +91,21 @@ public class SegmentationTrainer extends AbstractTrainer{
 
             System.out.println(refFiles.length + " tei files");
 
-			// the file for writing the training data
-			OutputStream os2 = null;
-			Writer writer2 = null;
-			if (trainingOutputPath != null) {
-				os2 = new FileOutputStream(trainingOutputPath);
-				writer2 = new OutputStreamWriter(os2, "UTF8");
-			}
-		
-			// the file for writing the evaluation data
-			OutputStream os3 = null;
-			Writer writer3 = null;
-			if (evalOutputPath != null) {
-				os3 = new FileOutputStream(evalOutputPath);
-				writer3 = new OutputStreamWriter(os3, "UTF8");
-			}
+            // the file for writing the training data
+            OutputStream os2 = null;
+            Writer writer2 = null;
+            if (trainingOutputPath != null) {
+                os2 = new FileOutputStream(trainingOutputPath);
+                writer2 = new OutputStreamWriter(os2, "UTF8");
+            }
+
+            // the file for writing the evaluation data
+            OutputStream os3 = null;
+            Writer writer3 = null;
+            if (evalOutputPath != null) {
+                os3 = new FileOutputStream(evalOutputPath);
+                writer3 = new OutputStreamWriter(os3, "UTF8");
+            }
 
             // get a factory for SAX parser
             SAXParserFactory spf = SAXParserFactory.newInstance();
@@ -130,16 +123,16 @@ public class SegmentationTrainer extends AbstractTrainer{
 
                 List<String> labeled = parser2.getLabeledResult();
                 //totalExamples += parser2.n;
-                
-				// we can now add the features
+
+                // we can now add the features
                 // we open the featured file
-				File theRawFile = new File(sourceRawPathLabel + File.separator + name.replace(".tei.xml", ""));
-				if (!theRawFile.exists()) {
-	                System.out.println("Raw file " + theRawFile +
-	                        " does not exist. Please have a look!");
-					continue;
-				}
-				
+                File theRawFile = new File(sourceRawPathLabel + File.separator + name.replace(".tei.xml", ""));
+                if (!theRawFile.exists()) {
+                    System.out.println("Raw file " + theRawFile +
+                            " does not exist. Please have a look!");
+                    continue;
+                }
+
                 int q = 0;
                 BufferedReader bis = new BufferedReader(
                         new InputStreamReader(new FileInputStream(
@@ -176,44 +169,45 @@ public class SegmentationTrainer extends AbstractTrainer{
                     }
                 }
                 bis.close();
-				
-				if ( (writer2 == null) && (writer3 != null) )
-					writer3.write(segmentation.toString() + "\n");
-				if ( (writer2 != null) && (writer3 == null) )
-					writer2.write(segmentation.toString() + "\n");
-				else {		
-					if (Math.random() <= splitRatio)
-						writer2.write(segmentation.toString() + "\n");
-					else 
-						writer3.write(segmentation.toString() + "\n");
-				}
+
+                if ((writer2 == null) && (writer3 != null))
+                    writer3.write(segmentation.toString() + "\n");
+                if ((writer2 != null) && (writer3 == null))
+                    writer2.write(segmentation.toString() + "\n");
+                else {
+                    if (Math.random() <= splitRatio)
+                        writer2.write(segmentation.toString() + "\n");
+                    else
+                        writer3.write(segmentation.toString() + "\n");
+                }
             }
 
             if (writer2 != null) {
-				writer2.close();
-				os2.close();
-			}
-			
-			if (writer3 != null) {
-				writer3.close();
-				os3.close();
-			}
+                writer2.close();
+                os2.close();
+            }
+
+            if (writer3 != null) {
+                writer3.close();
+                os3.close();
+            }
         } catch (Exception e) {
             throw new GrobidException("An exception occured while running Grobid.", e);
         }
         return totalExamples;
     }
-	
+
     /**
      * Add the selected features to the author model training for headers
+     *
      * @param sourceTEIPathLabel path to TEI files
      * @param sourceRawPathLabel path to raw files
-     * @param outputPath output train file
+     * @param outputPath         output train file
      * @return number of examples
      */
     public int addFeaturesSegmentation2(String sourceTEIPathLabel,
-                                   String sourceRawPathLabel,
-                                   File outputPath) {
+                                        String sourceRawPathLabel,
+                                        File outputPath) {
         int totalExamples = 0;
         try {
             System.out.println("sourceTEIPathLabel: " + sourceTEIPathLabel);
@@ -312,11 +306,11 @@ public class SegmentationTrainer extends AbstractTrainer{
      * Command line execution.
      *
      * @param args Command line arguments.
-     * @throws Exception 
+     * @throws Exception
      */
     public static void main(String[] args) throws Exception {
-    	MockContext.setInitialContext();
-    	GrobidProperties.getInstance();
+        MockContext.setInitialContext();
+        GrobidProperties.getInstance();
         AbstractTrainer.runTraining(new SegmentationTrainer());
         AbstractTrainer.runEvaluation(new SegmentationTrainer());
         MockContext.destroyInitialContext();
