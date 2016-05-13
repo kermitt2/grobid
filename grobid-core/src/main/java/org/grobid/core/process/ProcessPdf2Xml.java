48afecc8c9ef0098b457bb7986a77d2893044a05
Luca Foppiano <luca@foppiano.org>
2016-05-13 18:24:18 +0200

    Improved feedback in case of backend errors: issue #100. Removed redirect to /dev/null. Adding error code 126 (pdf2xml not found) and 127 (libxml2 not installed) in order to improve the feedback.

--- grobid-core/src/main/java/org/grobid/core/process/ProcessPdf2Xml.java
+++ grobid-core/src/main/java/org/grobid/core/process/ProcessPdf2Xml.java
@@ -1,51 +1,54 @@
 package org.grobid.core.process;
 
-import java.io.IOException;
-
 import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
 import java.util.List;
-import java.util.ArrayList;
 
 public class ProcessPdf2Xml {
 
-	private static final Logger LOGGER = LoggerFactory
-			.getLogger(ProcessPdf2Xml.class);
+    private static final Logger LOGGER = LoggerFactory
+            .getLogger(ProcessPdf2Xml.class);
 
-	/**
-	 * Process the conversion.
-	 */
-	public static Integer process(List<String> cmd) {
-		Process process = null;
-		ProcessBuilder builder = null;
-		Integer exit = null;
-		try {
-			builder = new ProcessBuilder(cmd);
-			process = builder.start();
-			exit = process.waitFor();
-				
-		} catch (InterruptedException ignore) {
-			// Process needs to be destroyed -- it's done in the finally block
-		} catch (IOException ioExp) {
-			LOGGER.error("IOException while launching the command {} : {}",
-					cmd, ioExp.getMessage());
-		} finally {
-			if (process != null) {
-				IOUtils.closeQuietly(process.getInputStream());
-				IOUtils.closeQuietly(process.getOutputStream());
+    /**
+     * Process the conversion.
+     */
+    public static Integer process(List<String> cmd) {
+        Process process = null;
+        ProcessBuilder builder = null;
+        Integer exit = null;
+        String message = "error message cannot be retrieved";
+        try {
+            builder = new ProcessBuilder(cmd);
+            process = builder.start();
+            exit = process.waitFor();
+            message = IOUtils.toString(process.getErrorStream());
 
-				IOUtils.closeQuietly(process.getErrorStream());
+        } catch (InterruptedException ignore) {
+            // Process needs to be destroyed -- it's done in the finally block
+            LOGGER.warn("pdf2xml process is going to be destroyed.");
+        } catch (IOException ioExp) {
+            LOGGER.error("IOException while launching the command {} : {}",
+                    cmd, ioExp.getMessage());
+        } finally {
+            if (process != null) {
+                IOUtils.closeQuietly(process.getInputStream());
+                IOUtils.closeQuietly(process.getOutputStream());
 
-				process.destroy();
+                IOUtils.closeQuietly(process.getErrorStream());
 
-				if (exit == null || exit != 0) {
-					LOGGER.error("pdftoxml process finished with error code: "
-							+ exit + ". " + cmd);
-				}
-			}
-		}
-		return exit;
-	}
+                process.destroy();
+
+                if (exit == null || exit != 0) {
+                    LOGGER.error("pdftoxml process finished with error code: "
+                            + exit + ". " + cmd);
+                    LOGGER.error("pdftoxml return message: \n" + message);
+                }
+            }
+        }
+        return exit;
+    }
 
 }
