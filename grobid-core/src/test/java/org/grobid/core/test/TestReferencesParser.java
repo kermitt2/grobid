00fd76705ebc11b3c43f236ad34859fa95da8b50
Luca Foppiano <luca@foppiano.org>
2016-09-21 15:15:18 +0200

    Replaced hardcoded / with File.separator in order to support different OS like Windows
    Former-commit-id: 5802080b5d24a28afbc90a0062419ea3cb26a38d

--- grobid-core/src/test/java/org/grobid/core/test/TestReferencesParser.java
+++ grobid-core/src/test/java/org/grobid/core/test/TestReferencesParser.java
@@ -1,15 +1,15 @@
 package org.grobid.core.test;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertThat;
+import org.grobid.core.data.BibDataSet;
+import org.grobid.core.main.GrobidConstants;
+import org.junit.Test;
 
 import java.io.File;
 import java.util.List;
 
-import org.grobid.core.data.BibDataSet;
-import org.grobid.core.main.GrobidConstants;
-import org.junit.Test;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
 
 /**
  * @author Patrice Lopez
@@ -19,21 +19,16 @@ public class TestReferencesParser extends EngineTest {
     private String getTestResourcePath() {
         return GrobidConstants.TEST_RESOURCES_PATH;
     }
-    
+
     @Test
     public void testReferences() throws Exception {
         String testPath = getTestResourcePath();
 
-        String pdfPath = testPath + "/Wang-paperAVE2008.pdf";
+        String pdfPath = testPath + File.separator + "Wang-paperAVE2008.pdf";
         List<BibDataSet> resRefs = engine.processReferences(new File(pdfPath), true);
 
         assertNotNull(resRefs);
         assertThat(resRefs.size(), is(12));
-
-        /*for(BibDataSet ref : resRefs) {
-            System.out.println(ref.getRawBib());
-            System.out.println(ref.getResBib().toTEI(0));
-        }*/
     }
 
 }
