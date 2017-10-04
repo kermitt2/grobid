3f33389a602abb514c90f35731ef239fe6c10b2b
lopez <patrice.lopez@science-miner.com>
2017-10-04 08:17:06 +0200

    convenience method
    Former-commit-id: 1e9dabd47e5ea645a6a37b4ee9afbe0d0d3fd5b8

--- grobid-core/src/main/java/org/grobid/core/lang/Language.java
+++ grobid-core/src/main/java/org/grobid/core/lang/Language.java
@@ -13,12 +13,29 @@ public final class Language {
     public static final String EN = "en";
     public static final String DE = "de";
     public static final String FR = "fr";
+    public static final String IT = "it";
+    public static final String ES = "es";
 
     private String lang;
     private double conf;
 
 	// default construction for jackson mapping
 	public Language() {}
+
+    public Language(String langId) {
+        if (langId == null) {
+            throw new GrobidException("Language id cannot be null");
+        }
+
+        if ((langId.length() != 3 && langId.length() != 2 && (!langId.equals("sorb")) && 
+            (!langId.equals("zh-cn")) && (!langId.equals("zh-tw"))) || !(Character.isLetter(langId.charAt(0))
+            && Character.isLetter(langId.charAt(1)))) {
+            throw new GrobidException("Language id should consist of two or three letters, but was: " + langId);
+        }
+
+        this.lang = langId;
+        this.conf = 1.0;
+    }
 
     public Language(String langId, double confidence) {
         if (langId == null) {
