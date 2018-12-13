5175086c92ec3284c5c496b9b48c047023aac00f
lopez <patrice.lopez@science-miner.com>
2018-12-13 18:11:47 +0100

    working glutton consolidation
    Former-commit-id: 459a007e45491c0003dff1488a68e62f63bf5872

--- grobid-core/src/main/java/org/grobid/core/utilities/crossref/CrossrefDeserializer.java
+++ grobid-core/src/main/java/org/grobid/core/utilities/crossref/CrossrefDeserializer.java
@@ -56,23 +56,26 @@ public abstract class CrossrefDeserializer<T extends Object> extends JsonDeseria
 	 */
 	protected ArrayNode normalizeResults(JsonParser parser) throws IOException {
 		JsonNode treeNode = parser.readValueAsTree();
-		
+		ArrayNode results = null;
+
 		JsonNode messageNode = treeNode.get("message");
 		
-		if (messageNode == null || !messageNode.isObject())
-			throw new ClientProtocolException("No message found in json result.");
-		
-		ObjectNode message = (ObjectNode)messageNode;
-		
-		JsonNode itemsNode = message.get("items");
-		ArrayNode results = null;
-		
-		if (itemsNode == null || !itemsNode.isArray()) {
+		if (messageNode == null || !messageNode.isObject()) {
+			//throw new ClientProtocolException("No message found in json result.");
+			// glutton
 			results = mapper.createArrayNode();
-			results.add(message);
-		} 
-		else
-			results = (ArrayNode)itemsNode;
+			results.add(treeNode);
+		} else {
+			ObjectNode message = (ObjectNode)messageNode;
+			JsonNode itemsNode = message.get("items");
+			
+			if (itemsNode == null || !itemsNode.isArray()) {
+				results = mapper.createArrayNode();
+				results.add(message);
+			} 
+			else
+				results = (ArrayNode)itemsNode;
+		}
 		
 		return results;
 	}
