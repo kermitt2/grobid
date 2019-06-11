d8bcaa164813007864fbdaceafab648969b31207
Luca Foppiano <luca@foppiano.org>
2019-06-11 16:07:36 +0900

    Adding the possibility to run jep via a virtual environment (ideally anaconda) #409

--- grobid-core/src/main/java/org/grobid/core/utilities/GrobidPropertyKeys.java
+++ grobid-core/src/main/java/org/grobid/core/utilities/GrobidPropertyKeys.java
@@ -106,4 +106,7 @@ public interface GrobidPropertyKeys {
      * The defined paths to create.
      */
     String[] PATHES_TO_CREATE = {PROP_TMP_PATH};
+
+
+    String PYTHON_VIRTUALENV_DIRECTORY = "grobid.delft.python.virtualEnv";
 }
