794bba84a7a960e0618244974abad5f62ff69954
Daniel Ecer <de-code@users.noreply.github.com>
2019-07-26 17:21:54 +0100

    extracted trainingData

--- grobid-core/src/test/java/org/grobid/core/jni/DeLFTModelTest.java
+++ grobid-core/src/test/java/org/grobid/core/jni/DeLFTModelTest.java
@@ -13,6 +13,8 @@ import org.grobid.core.utilities.GrobidPropertyKeys;
 
 
 public class DeLFTModelTest {
+    private File trainingData = new File("test/train.data");
+
     @Before
     public void setUp() {
         GrobidProperties.getInstance();
@@ -23,12 +25,11 @@ public class DeLFTModelTest {
 
     @Test
     public void testShouldBuildTrainCommand() {
-        File trainingData = new File("test/train.data");
         assertThat(
             DeLFTModel.getTrainCommand("model1", trainingData),
             contains(
                 "python3", "grobidTagger.py", "model1", "train",
-                "--input", trainingData.getAbsolutePath(),
+                "--input", this.trainingData.getAbsolutePath(),
                 "--output", GrobidProperties.getModelPath().getAbsolutePath()
             )
         );
@@ -37,12 +38,11 @@ public class DeLFTModelTest {
     @Test
     public void testShouldAddUseELMO() {
         GrobidProperties.getProps().put(GrobidPropertyKeys.PROP_GROBID_DELFT_ELMO, "true");
-        File trainingData = new File("test/train.data");
         assertThat(
             DeLFTModel.getTrainCommand("model1", trainingData),
             contains(
                 "python3", "grobidTagger.py", "model1", "train",
-                "--input", trainingData.getAbsolutePath(),
+                "--input", this.trainingData.getAbsolutePath(),
                 "--output", GrobidProperties.getModelPath().getAbsolutePath(),
                 "--use-ELMo"
             )
@@ -54,12 +54,11 @@ public class DeLFTModelTest {
         GrobidProperties.getProps().put(
             GrobidPropertyKeys.PROP_GROBID_DELFT_TRAIN_MODULE, "module1.py"
         );
-        File trainingData = new File("test/train.data");
         assertThat(
             DeLFTModel.getTrainCommand("model1", trainingData),
             contains(
                 "python3", "module1.py", "model1", "train",
-                "--input", trainingData.getAbsolutePath(),
+                "--input", this.trainingData.getAbsolutePath(),
                 "--output", GrobidProperties.getModelPath().getAbsolutePath()
             )
         );
@@ -68,12 +67,11 @@ public class DeLFTModelTest {
     @Test
     public void testShouldAddSingleCustomTrainArg() {
         GrobidProperties.getProps().put(GrobidPropertyKeys.PROP_GROBID_DELFT_TRAIN_ARGS, "arg1");
-        File trainingData = new File("test/train.data");
         assertThat(
             DeLFTModel.getTrainCommand("model1", trainingData),
             contains(
                 "python3", "grobidTagger.py", "model1", "train",
-                "--input", trainingData.getAbsolutePath(),
+                "--input", this.trainingData.getAbsolutePath(),
                 "--output", GrobidProperties.getModelPath().getAbsolutePath(),
                 "arg1"
             )
@@ -85,12 +83,11 @@ public class DeLFTModelTest {
         GrobidProperties.getProps().put(
             GrobidPropertyKeys.PROP_GROBID_DELFT_TRAIN_ARGS, "arg1 arg2"
         );
-        File trainingData = new File("test/train.data");
         assertThat(
             DeLFTModel.getTrainCommand("model1", trainingData),
             contains(
                 "python3", "grobidTagger.py", "model1", "train",
-                "--input", trainingData.getAbsolutePath(),
+                "--input", this.trainingData.getAbsolutePath(),
                 "--output", GrobidProperties.getModelPath().getAbsolutePath(),
                 "arg1",
                 "arg2"
