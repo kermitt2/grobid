979c1f9c54aa784f61addae6bb62cd5094ffc525
Luca Foppiano <luca@foppiano.org>
2019-08-20 18:05:05 +0900

    Adding more tests and moving code around
    Former-commit-id: ce933aa5ad7079deb10cc282e2e2d9ed5a4fa9b4

--- grobid-trainer/src/main/java/org/grobid/trainer/evaluation/EvaluationUtilities.java
+++ grobid-trainer/src/main/java/org/grobid/trainer/evaluation/EvaluationUtilities.java
@@ -124,59 +124,19 @@ public class EvaluationUtilities {
         return computeStats(theResult);
     }
 
+    /** Computes the stats for a single model, returning a ModelStats object, which
+     * ships:
+     *  - field level statistics
+     *  - instances statistics
+     */
     public static ModelStats computeStats(String theResult) {
-        ModelStats modelStats = new ModelStats();
-        modelStats.setRawResults(theResult);
-        // report token-level results
-//        Stats wordStats = tokenLevelStats(theResult);
-//        modelStats.setTokenStats(wordStats);
-
-        // report field-level results
-        Stats fieldStats = fieldLevelStats(theResult);
-        modelStats.setFieldStats(fieldStats);
-
-        // instance-level: instances are separated by a new line in the result file
-        // third pass
-        theResult = theResult.replace("\n\n", "\n \n");
-        StringTokenizer stt = new StringTokenizer(theResult, "\n");
-        boolean allGood = true;
-        int correctInstance = 0;
-        int totalInstance = 0;
-        String line = null;
-        while (stt.hasMoreTokens()) {
-            line = stt.nextToken();
-            if ((line.trim().length() == 0) || (!stt.hasMoreTokens())) {
-                // instance done
-                totalInstance++;
-                if (allGood) {
-                    correctInstance++;
-                }
-                // we reinit for a new instance
-                allGood = true;
-            } else {
-                StringTokenizer st = new StringTokenizer(line, "\t ");
-                String obtainedLabel = null;
-                String expectedLabel = null;
-                while (st.hasMoreTokens()) {
-                    obtainedLabel = getPlainLabel(st.nextToken());
-                    if (st.hasMoreTokens()) {
-                        expectedLabel = obtainedLabel;
-                    }
-                }
-
-                if (!obtainedLabel.equals(expectedLabel)) {
-                    // one error is enough to have the whole instance false, damn!
-                    allGood = false;
-                }
-            }
-        }
-
-        modelStats.setTotalInstances(totalInstance);
-        modelStats.setCorrectInstance(correctInstance);
-
-        return modelStats;
+        return new ModelStats(theResult);
     }
 
+    /**
+     * computes the token level results
+     */
+    @Deprecated
     public static Stats tokenLevelStats(String theResult) {
         Stats wordStats = new Stats();
         String line = null;
@@ -210,131 +170,6 @@ public class EvaluationUtilities {
 			}*/
         }
         return wordStats;
-    }
-
-    public static Stats fieldLevelStats(String theResult) {
-        Stats fieldStats = new Stats();
-
-        // field: a field is simply a sequence of token with the same label
-
-        // we build first the list of fields in expected and obtained result
-        // with offset positions
-        List<Pair<String, OffsetPosition>> expectedFields = new ArrayList<>();
-        List<Pair<String, OffsetPosition>> obtainedFields = new ArrayList<>();
-        StringTokenizer stt = new StringTokenizer(theResult, System.lineSeparator());
-        String line = null;
-        String previousExpectedLabel = null;
-        String previousObtainedLabel = null;
-        int pos = 0; // current token index
-        OffsetPosition currentObtainedPosition = new OffsetPosition();
-        currentObtainedPosition.start = 0;
-        OffsetPosition currentExpectedPosition = new OffsetPosition();
-        currentExpectedPosition.start = 0;
-        String obtainedLabel = null;
-        String expectedLabel = null;
-        while (stt.hasMoreTokens()) {
-            line = stt.nextToken();
-            obtainedLabel = null;
-            expectedLabel = null;
-            StringTokenizer st = new StringTokenizer(line, "\t ");
-            while (st.hasMoreTokens()) {
-                obtainedLabel = st.nextToken();
-                if (st.hasMoreTokens()) {
-                    expectedLabel = obtainedLabel;
-                }
-            }
-
-            if ((obtainedLabel == null) || (expectedLabel == null))
-                continue;
-
-            if ((previousObtainedLabel != null) &&
-                (!obtainedLabel.equals(getPlainLabel(previousObtainedLabel)))) {
-                // new obtained field
-                currentObtainedPosition.end = pos - 1;
-                Pair<String, OffsetPosition> theField = new Pair<>(getPlainLabel(previousObtainedLabel),
-                    currentObtainedPosition);
-                currentObtainedPosition = new OffsetPosition();
-                currentObtainedPosition.start = pos;
-                obtainedFields.add(theField);
-            }
-
-            if ((previousExpectedLabel != null) &&
-                (!expectedLabel.equals(getPlainLabel(previousExpectedLabel)))) {
-                // new expected field
-                currentExpectedPosition.end = pos - 1;
-                Pair<String, OffsetPosition> theField = new Pair<>(getPlainLabel(previousExpectedLabel),
-                    currentExpectedPosition);
-                currentExpectedPosition = new OffsetPosition();
-                currentExpectedPosition.start = pos;
-                expectedFields.add(theField);
-            }
-
-            previousExpectedLabel = expectedLabel;
-            previousObtainedLabel = obtainedLabel;
-            pos++;
-        }
-        // last fields of the sequence
-        if ((previousObtainedLabel != null)) {
-            currentObtainedPosition.end = pos - 1;
-            Pair<String, OffsetPosition> theField = new Pair<>(getPlainLabel(previousObtainedLabel),
-                currentObtainedPosition);
-            obtainedFields.add(theField);
-        }
-
-        if ((previousExpectedLabel != null)) {
-            currentExpectedPosition.end = pos - 1;
-            Pair<String, OffsetPosition> theField = new Pair<>(getPlainLabel(previousExpectedLabel),
-                currentExpectedPosition);
-            expectedFields.add(theField);
-        }
-
-        // we then simply compared the positions and labels of the two fields and update
-        // statistics
-        int obtainedFieldIndex = 0;
-        List<Pair<String, OffsetPosition>> matchedObtainedFields = new ArrayList<Pair<String, OffsetPosition>>();
-        for (Pair<String, OffsetPosition> expectedField : expectedFields) {
-            expectedLabel = expectedField.getA();
-            int expectedStart = expectedField.getB().start;
-            int expectedEnd = expectedField.getB().end;
-
-            LabelStat labelStat = fieldStats.getLabelStat(getPlainLabel(expectedLabel));
-            labelStat.incrementExpected();
-
-            // try to find a match in the obtained fields
-            boolean found = false;
-            for (int i = obtainedFieldIndex; i < obtainedFields.size(); i++) {
-                obtainedLabel = obtainedFields.get(i).getA();
-                if (!expectedLabel.equals(obtainedLabel))
-                    continue;
-                if ((expectedStart == obtainedFields.get(i).getB().start) &&
-                    (expectedEnd == obtainedFields.get(i).getB().end)) {
-                    // we have a match
-                    labelStat.incrementObserved(); // TP
-                    found = true;
-                    obtainedFieldIndex = i;
-                    matchedObtainedFields.add(obtainedFields.get(i));
-                    break;
-                }
-                // if we went too far, we can stop the pain
-                if (expectedEnd < obtainedFields.get(i).getB().start) {
-                    break;
-                }
-            }
-            if (!found) {
-                labelStat.incrementFalseNegative();
-            }
-        }
-
-        // all the obtained fields without match in the expected fields are false positive
-        for (Pair<String, OffsetPosition> obtainedField : obtainedFields) {
-            if (!matchedObtainedFields.contains(obtainedField)) {
-                obtainedLabel = obtainedField.getA();
-                LabelStat labelStat = fieldStats.getLabelStat(getPlainLabel(obtainedLabel));
-                labelStat.incrementFalsePositive();
-            }
-        }
-
-        return fieldStats;
     }
 
 
