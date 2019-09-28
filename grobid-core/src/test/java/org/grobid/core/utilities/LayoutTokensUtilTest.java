3212b61d1eb3d99b118dae35074c26003c8d639d
lopez <patrice.lopez@science-miner.com>
2019-09-28 19:40:36 +0200

    fix test

--- grobid-core/src/test/java/org/grobid/core/utilities/LayoutTokensUtilTest.java
+++ grobid-core/src/test/java/org/grobid/core/utilities/LayoutTokensUtilTest.java
@@ -8,8 +8,6 @@ import org.junit.Test;
 import java.util.List;
 import java.util.stream.IntStream;
 
-import static org.hamcrest.Matchers.is;
-
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.Matchers.hasSize;
 import static org.junit.Assert.assertThat;
