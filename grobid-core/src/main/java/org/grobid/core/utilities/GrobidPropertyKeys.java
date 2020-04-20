667794ea553c783f3db0641004199bf2bb4568f8
Luca Foppiano <Foppiano.Luca@nims.go.jp>
2020-04-20 20:03:05 +0900

    Correcting grobid.properties properties name

--- grobid-core/src/main/java/org/grobid/core/utilities/GrobidPropertyKeys.java
+++ grobid-core/src/main/java/org/grobid/core/utilities/GrobidPropertyKeys.java
@@ -8,7 +8,7 @@ public interface GrobidPropertyKeys {
 
     String PROP_GROBID_IS_CONTEXT_SERVER = "grobid.is.context.server";
 
-    String PROP_GROBID_VERSION = "org.grobid.version";
+    String PROP_GROBID_VERSION = "grobid.version";
     String PROP_TMP_PATH = "grobid.temp.path";
     //    public static final String PROP_BIN_PATH = "grobid.bin.path";
     String PROP_NATIVE_LIB_PATH = "grobid.nativelibrary.path";
@@ -34,14 +34,14 @@ public interface GrobidPropertyKeys {
      * If indicated, include a "mailto" parameter in the crossref query and User-Agent 
      * header, as recommended by CrossRef REST API documentation. 
      */
-    String PROP_CROSSREF_MAILTO = "org.grobid.crossref.mailto";
+    String PROP_CROSSREF_MAILTO = "grobid.crossref.mailto";
 
     /** 
      * For indicating a Crossref Metadata Plus authorization token to be used for Crossref
      * requests for the subscribers of this service.  This token will ensure that said 
      * requests get directed to a pool of machines that are reserved for "Plus" SLA users.
      */
-    String PROP_CROSSREF_TOKEN = "org.grobid.crossref.token";
+    String PROP_CROSSREF_TOKEN = "grobid.crossref.token";
 
     /*String PROP_MYSQL_HOST = "grobid.mysql_host";
     String PROP_MYSQL_PORT = "grobid.mysql_port";
@@ -57,11 +57,11 @@ public interface GrobidPropertyKeys {
     String PROP_PDF_BLOCKS_MAX = "grobid.pdf.blocks.max";
     String PROP_PDF_TOKENS_MAX = "grobid.pdf.tokens.max";
 
-    String PROP_GROBID_MAX_CONNECTIONS = "org.grobid.max.connections";
-    String PROP_GROBID_POOL_MAX_WAIT = "org.grobid.pool.max.wait";
+    String PROP_GROBID_MAX_CONNECTIONS = "grobid.max.connections";
+    String PROP_GROBID_POOL_MAX_WAIT = "grobid.pool.max.wait";
 
-    String PROP_GLUTTON_HOST = "org.grobid.glutton.host";
-    String PROP_GLUTTON_PORT = "org.grobid.glutton.port";
+    String PROP_GLUTTON_HOST = "grobid.glutton.host";
+    String PROP_GLUTTON_PORT = "grobid.glutton.port";
 
     /**
      * Determines if properties like the firstnames, lastnames country codes and
@@ -74,30 +74,30 @@ public interface GrobidPropertyKeys {
      * The name of the env-entry located in the web.xml, via which the
      * grobid-service.propeties path is set.
      */
-    String PROP_GROBID_HOME = "org.grobid.home";
+    String PROP_GROBID_HOME = "grobid.home";
 
     /**
      * The name of the env-entry located in the web.xml, via which the
      * grobid.propeties path is set.
      */
-    String PROP_GROBID_PROPERTY = "org.grobid.property";
+    String PROP_GROBID_PROPERTY = "grobid.property";
 
     /**
      * The name of the system property, via which the grobid home folder can be
      * located.
      */
-//    String PROP_GROBID_SERVICE_PROPERTY = "org.grobid.property.service";
+//    String PROP_GROBID_SERVICE_PROPERTY = "grobid.property.service";
 
     /**
      * name of the property setting the admin password
      */
-    String PROP_GROBID_SERVICE_ADMIN_PW = "org.grobid.service.admin.pw";
+    String PROP_GROBID_SERVICE_ADMIN_PW = "grobid.service.admin.pw";
 
     /**
      * If set to true, parallel execution will be done, else a queuing of
      * requests will be done.
      */
-    String PROP_GROBID_SERVICE_IS_PARALLEL_EXEC = "org.grobid.service.is.parallel.execution";
+    String PROP_GROBID_SERVICE_IS_PARALLEL_EXEC = "grobid.service.is.parallel.execution";
 
     /**
      * Bibliographical data consolidation service to be used, either "crossref" for CrossRef 
