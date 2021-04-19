5db13ba9913db3c73781472c1806b90bf8739d94
Aazhar <achrafazhargithub@gmail.com>
2021-04-19 15:36:13 +0200

    Revert "reactivate postvalidation for crossref consolidation without doi."
    This reverts commit 2acd1faf87c90e73c586c4071ab88ad862ffaffe.

--- grobid-core/src/main/java/org/grobid/core/utilities/Consolidation.java
+++ grobid-core/src/main/java/org/grobid/core/utilities/Consolidation.java
@@ -657,17 +657,17 @@ public class Consolidation {
         boolean valid = true;
 
         // check main metadata available in source with fuzzy matching
-        if (!StringUtils.isBlank(source.getTitle()) && !StringUtils.isBlank(source.getTitle())) {
+        /*if (!StringUtils.isBlank(source.getTitle()) && !StringUtils.isBlank(source.getTitle())) {
 //System.out.println(source.getTitle() + " / " + result.getTitle() + " = " + ratcliffObershelpDistance(source.getTitle(), result.getTitle(), false));      
-            if (ratcliffObershelpDistance(source.getTitle(), result.getTitle(), false) < 0.7)
+            if (ratcliffObershelpDistance(source.getTitle(), result.getTitle(), false) < 0.8)
                 return false;
-        }
+        }*/
 
         if (!StringUtils.isBlank(source.getFirstAuthorSurname()) && 
             !StringUtils.isBlank(result.getFirstAuthorSurname())) {
 //System.out.println(source.getFirstAuthorSurname() + " / " + result.getFirstAuthorSurname() + " = " + 
 //    ratcliffObershelpDistance(source.getFirstAuthorSurname(), result.getFirstAuthorSurname(), false)); 
-            if (ratcliffObershelpDistance(source.getFirstAuthorSurname(),result.getFirstAuthorSurname(), false) < 0.7)
+            if (ratcliffObershelpDistance(source.getFirstAuthorSurname(),result.getFirstAuthorSurname(), false) < 0.8)
                 return false;
         }
 
