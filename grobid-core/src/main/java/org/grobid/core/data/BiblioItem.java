d980ec946f9f4b064638789fe3412329be27a787
Oliver Kopp <kopp.dev@gmail.com>
2021-06-07 23:56:01 +0200

    Fix typo in param description in BiblioItem.java

--- grobid-core/src/main/java/org/grobid/core/data/BiblioItem.java
+++ grobid-core/src/main/java/org/grobid/core/data/BiblioItem.java
@@ -1878,7 +1878,7 @@ public class BiblioItem {
     /**
      * Export to BibTeX format
      *
-     * @param id the BibTeX ke to use.
+     * @param id the BibTeX key to use.
      */
     public String toBibTeX(String id) {
         return toBibTeX(id, new GrobidAnalysisConfig.GrobidAnalysisConfigBuilder().includeRawCitations(false).build());
@@ -1887,7 +1887,7 @@ public class BiblioItem {
     /**
      * Export to BibTeX format
      *
-     * @param id                  the BibTeX ke to use
+     * @param id the BibTeX key to use
      */
     public String toBibTeX(String id, GrobidAnalysisConfig config) {
         String type;
