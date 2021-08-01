2a69fa4b3c9ca881438ed5b3bf5a3c50189aa6d0
Benedikt Tutzer <benedikt.tutzer@tuwien.ac.at>
2021-08-01 16:16:36 +0200

    Fix empty lastname
    If only a firstname is detected, use it without lastname.

--- grobid-core/src/main/java/org/grobid/core/data/BiblioItem.java
+++ grobid-core/src/main/java/org/grobid/core/data/BiblioItem.java
@@ -1922,12 +1922,19 @@ public class BiblioItem {
                     fullAuthors.stream()
                                .filter(person -> person != null)
                                .forEachOrdered(person -> {
-                                   String author = person.getLastName();
+                                   String author = "";
+                                   if (person.getLastName() != null) {
+                                       author = person.getLastName();
+                                   }
                                    if (person.getFirstName() != null) {
-                                       author += ", ";
+                                       if (author.length() > 0) {
+                                           author += ", ";
+                                       }
                                        author += person.getFirstName();
                                    }
-                                   authors.add(author);
+                                   if (author.length() > 0 ) {
+                                       authors.add(author);
+                                   }
                                });
                 } else if (this.authors != null) {
                     StringTokenizer st = new StringTokenizer(this.authors, ";");
