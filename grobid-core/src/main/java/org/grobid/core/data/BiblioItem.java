9a41c570622fc242eb406a8638fd6a87f679c565
lopez <patrice.lopez@science-miner.com>
2021-08-18 08:27:21 +0200

    addition from #814

--- grobid-core/src/main/java/org/grobid/core/data/BiblioItem.java
+++ grobid-core/src/main/java/org/grobid/core/data/BiblioItem.java
@@ -1990,8 +1990,18 @@ public class BiblioItem {
                 if (isoDate != null) {
                     bibtex.add("  date = {" + isoDate + "}");
                 }
-            }
-            if (publication_date != null) {
+                if (normalized_publication_date.getYear() >= 0) {
+                    bibtex.add("  year = {" + normalized_publication_date.getYear() + "}");
+                
+                    if (normalized_publication_date.getMonth() >= 0) {
+                        bibtex.add("  month = {" + normalized_publication_date.getMonth() + "}");
+                    
+                        if (normalized_publication_date.getDay() >= 0) {
+                            bibtex.add("  day = {" + normalized_publication_date.getDay() + "}");
+                        }
+                    }
+                }
+            } else if (publication_date != null) {
                 bibtex.add("  year = {" + publication_date + "}");
             }
 
