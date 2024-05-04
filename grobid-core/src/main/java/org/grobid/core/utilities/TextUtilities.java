ec52f13948f854fc28c44f4a37f357c1ee9f44b2
Luca Foppiano <Foppiano.Luca@nims.go.jp>
2024-05-04 12:50:26 +0900

    get fixes on matchTokenAndString from PR #1099

--- grobid-core/src/main/java/org/grobid/core/utilities/TextUtilities.java
+++ grobid-core/src/main/java/org/grobid/core/utilities/TextUtilities.java
@@ -1557,22 +1557,25 @@ public class TextUtilities {
             return null;
     }
 
-    public static List<OffsetPosition> matchTokenAndString(List<LayoutToken> layoutTokens, String text, List<OffsetPosition> urlPositions) {
+    public static List<OffsetPosition> matchTokenAndString(List<LayoutToken> layoutTokens, String text, List<OffsetPosition> positions) {
         List<OffsetPosition> newPositions = new ArrayList<>();
         StringBuilder accumulator = new StringBuilder();
         int pos = 0;
+        int textPositionOfToken = 0;
 
-        for (OffsetPosition urlPosition : urlPositions) {
-            List<LayoutToken> urlTokens = layoutTokens.subList(urlPosition.start, urlPosition.end);
+        for (OffsetPosition position : positions) {
+            List<LayoutToken> annotationTokens = layoutTokens.subList(position.start, position.end);
             boolean first = true;
-            for (int i = 0; i < urlTokens.size(); i++) {
-                LayoutToken token = urlTokens.get(i);
+            accumulator = new StringBuilder();
+            for (int i = 0; i < annotationTokens.size(); i++) {
+                LayoutToken token = annotationTokens.get(i);
                 if (StringUtils.isEmpty(token.getText()))
                     continue;
-                int newPos = text.indexOf(token.getText(), pos);
-                if (newPos != -1) {
+                textPositionOfToken = text.indexOf(token.getText(), pos);
+                if (textPositionOfToken != -1) {
+                    //We update pos only at the first token of the annotation positions
                     if (first) {
-                        pos = newPos;
+                        pos = textPositionOfToken;
                         first = false;
                     }
                     accumulator.append(token);
@@ -1581,16 +1584,25 @@ public class TextUtilities {
                         continue;
                     }
                     if (StringUtils.isNotEmpty(accumulator)) {
+                        int accumulatorTextLength = accumulator.toString().length();
                         int start = text.indexOf(accumulator.toString(), pos);
-                        newPositions.add(new OffsetPosition(start, start + accumulator.toString().length()));
-                        accumulator = new StringBuilder();
-                        pos = newPos;
-                        first = true;
+                        int end = start + accumulatorTextLength;
+                        newPositions.add(new OffsetPosition(start, end));
+                        pos = end;
                         break;
                     }
-                    pos = newPos;
+                    pos = textPositionOfToken;
                 }
             }
+            if (StringUtils.isNotEmpty(accumulator)) {
+                int annotationTextLength = accumulator.toString().length();
+                int start = text.indexOf(accumulator.toString(), pos);
+                int end = start + annotationTextLength;
+                newPositions.add(new OffsetPosition(start, end));
+                pos = end;
+                accumulator = new StringBuilder();
+            }
+
         }
         if (StringUtils.isNotEmpty(accumulator)) {
             int start = text.indexOf(accumulator.toString(), pos);
