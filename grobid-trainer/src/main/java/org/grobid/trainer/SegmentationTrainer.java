1696743a891eb7c7e2aa063386f468d04fbea3d3
Luca Foppiano <luca@foppiano.org>
2024-07-28 09:17:00 +0300

    add segmentation trainer for the light model flavour

--- grobid-trainer/src/main/java/org/grobid/trainer/SegmentationTrainer.java
+++ grobid-trainer/src/main/java/org/grobid/trainer/SegmentationTrainer.java
@@ -4,21 +4,33 @@ import org.grobid.core.GrobidModels;
 import org.grobid.core.exceptions.GrobidException;
 import org.grobid.core.utilities.GrobidProperties;
 import org.grobid.core.utilities.UnicodeUtil;
+import org.grobid.trainer.sax.TEISegmentationArticleLightSaxParser;
 import org.grobid.trainer.sax.TEISegmentationSaxParser;
 
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 import org.apache.commons.io.FileUtils;
 
+import static org.grobid.core.GrobidModels.SEGMENTATION_ARTICLE_LIGHT;
+
 public class SegmentationTrainer extends AbstractTrainer {
+
+    private final GrobidModels.ModelFlavour flavour;
 
     public SegmentationTrainer() {
         super(GrobidModels.SEGMENTATION);
+        flavour = null;
+    }
+
+    public SegmentationTrainer(GrobidModels.ModelFlavour modelFlavour) {
+        super(GrobidModels.getModelFlavour(GrobidModels.SEGMENTATION, modelFlavour));
+        flavour = modelFlavour;
     }
 
     @Override
@@ -91,7 +103,7 @@ public class SegmentationTrainer extends AbstractTrainer {
             Writer writer2 = null;
             if (trainingOutputPath != null) {
                 os2 = new FileOutputStream(trainingOutputPath);
-                writer2 = new OutputStreamWriter(os2, "UTF8");
+                writer2 = new OutputStreamWriter(os2, StandardCharsets.UTF_8);
             }
 
             // the file for writing the evaluation data
@@ -99,7 +111,7 @@ public class SegmentationTrainer extends AbstractTrainer {
             Writer writer3 = null;
             if (evalOutputPath != null) {
                 os3 = new FileOutputStream(evalOutputPath);
-                writer3 = new OutputStreamWriter(os3, "UTF8");
+                writer3 = new OutputStreamWriter(os3, StandardCharsets.UTF_8);
             }
 
             // get a factory for SAX parser
@@ -109,7 +121,12 @@ public class SegmentationTrainer extends AbstractTrainer {
                 String name = tf.getName();
                 LOGGER.info("Processing: " + name);
 
-                TEISegmentationSaxParser parser2 = new TEISegmentationSaxParser();
+                TEISegmentationSaxParser parser2;
+                if (flavour != null) {
+                    parser2 = new TEISegmentationArticleLightSaxParser();
+                } else {
+                    parser2 = new TEISegmentationSaxParser();
+                }
 
                 //get a new instance of parser
                 SAXParser p = spf.newSAXParser();
@@ -142,7 +159,7 @@ FileUtils.writeStringToFile(new File("/tmp/expected-"+name+".txt"), temp.toStrin
                 
                     int q = 0;
                     BufferedReader bis = new BufferedReader(
-                            new InputStreamReader(new FileInputStream(theRawFile), "UTF8"));
+                        new InputStreamReader(new FileInputStream(theRawFile), StandardCharsets.UTF_8));
                     StringBuilder segmentation = new StringBuilder();
                     String line = null;
                     int l = 0;
@@ -213,12 +230,16 @@ FileUtils.writeStringToFile(new File("/tmp/expected-"+name+".txt"), temp.toStrin
 
             if (writer2 != null) {
                 writer2.close();
-                os2.close();
+                if (os2 != null) {
+                    os2.close();
+                }
             }
 
             if (writer3 != null) {
                 writer3.close();
-                os3.close();
+                if (os3 != null) {
+                    os3.close();
+                }
             }
         } catch (Exception e) {
             throw new GrobidException("An exception occured while running Grobid.", e);
@@ -226,121 +247,13 @@ FileUtils.writeStringToFile(new File("/tmp/expected-"+name+".txt"), temp.toStrin
         return totalExamples;
     }
 
-    /**
-     * Add the selected features to the author model training for headers
-     *
-     * @param sourceTEIPathLabel path to TEI files
-     * @param sourceRawPathLabel path to raw files
-     * @param outputPath         output train file
-     * @return number of examples
-     */
-    /*public int addFeaturesSegmentation2(String sourceTEIPathLabel,
-                                        String sourceRawPathLabel,
-                                        File outputPath) {
-        int totalExamples = 0;
-        try {
-            System.out.println("sourceTEIPathLabel: " + sourceTEIPathLabel);
-            System.out.println("sourceRawPathLabel: " + sourceRawPathLabel);
-            System.out.println("outputPath: " + outputPath);
 
-            // we need first to generate the labeled files from the TEI annotated files
-            File input = new File(sourceTEIPathLabel);
-            // we process all tei files in the output directory
-            File[] refFiles = input.listFiles(new FilenameFilter() {
-                public boolean accept(File dir, String name) {
-                    return name.endsWith(".tei.xml");
-                }
-            });
-
-            if (refFiles == null) {
-                return 0;
-            }
-
-            System.out.println(refFiles.length + " tei files");
-
-            // the file for writing the training data
-            OutputStream os2 = new FileOutputStream(outputPath);
-            Writer writer2 = new OutputStreamWriter(os2, "UTF8");
-
-            // get a factory for SAX parser
-            SAXParserFactory spf = SAXParserFactory.newInstance();
-
-//            int n = 0;
-            for (File tf : refFiles) {
-                String name = tf.getName();
-                System.out.println(name);
-
-                TEISegmentationSaxParser parser2 = new TEISegmentationSaxParser();
-
-                //get a new instance of parser
-                SAXParser p = spf.newSAXParser();
-                p.parse(tf, parser2);
-
-                List<String> labeled = parser2.getLabeledResult();
-                //totalExamples += parser2.n;
-
-                // we can now add the features
-                // we open the featured file
-                int q = 0;
-                BufferedReader bis = new BufferedReader(
-                        new InputStreamReader(new FileInputStream(
-                                sourceRawPathLabel + File.separator + name.replace(".tei.xml", "")), "UTF8"));
-
-                StringBuilder segmentation = new StringBuilder();
-
-                String line;
-//                String lastTag = null;
-                while ((line = bis.readLine()) != null) {
-                    int ii = line.indexOf(' ');
-                    String token = null;
-                    if (ii != -1)
-                        token = line.substring(0, ii);
-//                    boolean found = false;
-                    // we get the label in the labelled data file for the same token
-                    for (int pp = q; pp < labeled.size(); pp++) {
-                        String localLine = labeled.get(pp);
-                        StringTokenizer st = new StringTokenizer(localLine, " ");
-                        if (st.hasMoreTokens()) {
-                            String localToken = st.nextToken();
-
-                            if (localToken.equals(token)) {
-                                String tag = st.nextToken();
-                                segmentation.append(line).append(" ").append(tag);
-//                                lastTag = tag;
-//                                found = true;
-                                q = pp + 1;
-                                pp = q + 10;
-                            }
-                        }
-                        if (pp - q > 5) {
-                            break;
-                        }
-                    }
-                }
-                bis.close();
-
-                // format with features for sequence tagging...
-                writer2.write(segmentation.toString() + "\n");
-            }
-
-            writer2.close();
-            os2.close();
-        } catch (Exception e) {
-            throw new GrobidException("An exception occured while running Grobid.", e);
-        }
-        return totalExamples;
-    }*/
-
-    /**
-     * Command line execution.
-     *
-     * @param args Command line arguments.
-     * @throws Exception
-     */
     public static void main(String[] args) throws Exception {
         GrobidProperties.getInstance();
-        AbstractTrainer.runTraining(new SegmentationTrainer());
-        System.out.println(AbstractTrainer.runEvaluation(new SegmentationTrainer()));
+        AbstractTrainer.runTraining(new SegmentationTrainer(GrobidModels.ModelFlavour.ARTICLE_LIGHT));
+        System.out.println(AbstractTrainer.runEvaluation(new SegmentationTrainer(GrobidModels.ModelFlavour.ARTICLE_LIGHT)));
+//        AbstractTrainer.runTraining(new SegmentationTrainer());
+//        System.out.println(AbstractTrainer.runEvaluation(new SegmentationTrainer()));
         System.exit(0);
     }
 }
---
+++ grobid-trainer/src/test/java/org/grobid/trainer/sax/TEISegmentationArticleLightSaxParserTest.java
@@ -0,0 +1,43 @@
+package org.grobid.trainer.sax;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import java.io.InputStream;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.Assert.assertThat;
+
+public class TEISegmentationArticleLightSaxParserTest {
+
+    TEISegmentationArticleLightSaxParser target;
+    SAXParserFactory spf;
+
+
+    @Before
+    public void setUp() throws Exception {
+        spf = SAXParserFactory.newInstance();
+        target = new TEISegmentationArticleLightSaxParser();
+    }
+
+    @Test
+    public void testSegmentation() throws Exception {
+        InputStream is = this.getClass().getResourceAsStream("/10.1371_journal.pone.0210128.training.segmentation.tei.xml");
+
+        final SAXParser p = spf.newSAXParser();
+        p.parse(is, target);
+
+        assertThat(target.getLabeledResult(), hasSize(greaterThan(0)));
+//        assertThat(target.getLabeledResult().get(0), hasSize(49));
+//        assertThat(target.getLabeledResult().get(0).get(0).toString(), is("I-<author>"));
+//        assertThat(target.getTokensResult().get(0).get(0).toString(), is("H"));
+//        assertThat(target.getLabeledResult().get(0).get(1).toString(), is("<author>"));
+//        assertThat(target.getTokensResult().get(0).get(1).toString(), is("."));
+
+    }
+
+}
