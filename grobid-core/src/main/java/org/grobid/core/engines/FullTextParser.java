c664d63d62219021f4deb0cc66c16329e8c7fdc3
Patrice Lopez <patrice.lopez@science-miner.com>
2024-09-24 14:07:08 +0200

    working version

--- grobid-core/src/main/java/org/grobid/core/engines/FullTextParser.java
+++ grobid-core/src/main/java/org/grobid/core/engines/FullTextParser.java
@@ -151,7 +151,12 @@ public class FullTextParser extends AbstractParser {
         }
         try {
 			// general segmentation
-			Document doc = parsers.getSegmentationParser().processing(documentSource, config);
+            Document doc = parsers.getFigureSegmenterParser().extract(documentSource, config);
+			doc = parsers.getSegmentationParser().processing(doc, config);
+
+
+
+
 			SortedSet<DocumentPiece> documentBodyParts = doc.getDocumentPart(SegmentationLabels.BODY);
 
             // header processing
